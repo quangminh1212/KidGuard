4f5873ccb9645ed79f775c82ec16059a757b31a2
Bach Minh Quang <0947556012abc@gmail.com>
2025-08-26 18:06:17 +0700

    UI: add basic dashboard to start/stop input monitoring; Hooking: implement low-level hooks with privacy-preserving counters

--- ChildGuard.UI/Form1.Designer.cs
+++ ChildGuard.UI/Form1.Designer.cs
@@ -7,6 +7,13 @@ partial class Form1
     /// </summary>
     private System.ComponentModel.IContainer components = null;
 
+    private System.Windows.Forms.Label lblKeys = null!;
+    private System.Windows.Forms.Label lblMouse = null!;
+    private System.Windows.Forms.Button btnStart = null!;
+    private System.Windows.Forms.Button btnStop = null!;
+    private System.Windows.Forms.CheckBox chkEnableInput = null!;
+    private System.Windows.Forms.Timer uiTimer = null!;
+
     /// <summary>
     ///  Clean up any resources being used.
     /// </summary>
@@ -29,9 +36,83 @@ partial class Form1
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
+        this.lblKeys = new System.Windows.Forms.Label();
+        this.lblMouse = new System.Windows.Forms.Label();
+        this.btnStart = new System.Windows.Forms.Button();
+        this.btnStop = new System.Windows.Forms.Button();
+        this.chkEnableInput = new System.Windows.Forms.CheckBox();
+        this.uiTimer = new System.Windows.Forms.Timer(this.components);
+        this.SuspendLayout();
+        // 
+        // lblKeys
+        // 
+        this.lblKeys.AutoSize = true;
+        this.lblKeys.Location = new System.Drawing.Point(24, 24);
+        this.lblKeys.Name = "lblKeys";
+        this.lblKeys.Size = new System.Drawing.Size(122, 15);
+        this.lblKeys.TabIndex = 0;
+        this.lblKeys.Text = "KeyPress Count: 0";
+        // 
+        // lblMouse
+        // 
+        this.lblMouse.AutoSize = true;
+        this.lblMouse.Location = new System.Drawing.Point(24, 54);
+        this.lblMouse.Name = "lblMouse";
+        this.lblMouse.Size = new System.Drawing.Size(139, 15);
+        this.lblMouse.TabIndex = 1;
+        this.lblMouse.Text = "Mouse Event Count: 0";
+        // 
+        // chkEnableInput
+        // 
+        this.chkEnableInput.AutoSize = true;
+        this.chkEnableInput.Location = new System.Drawing.Point(24, 92);
+        this.chkEnableInput.Name = "chkEnableInput";
+        this.chkEnableInput.Size = new System.Drawing.Size(187, 19);
+        this.chkEnableInput.TabIndex = 2;
+        this.chkEnableInput.Text = "Enable input monitoring (LL)";
+        this.chkEnableInput.UseVisualStyleBackColor = true;
+        // 
+        // btnStart
+        // 
+        this.btnStart.Location = new System.Drawing.Point(24, 127);
+        this.btnStart.Name = "btnStart";
+        this.btnStart.Size = new System.Drawing.Size(94, 29);
+        this.btnStart.TabIndex = 3;
+        this.btnStart.Text = "Start";
+        this.btnStart.UseVisualStyleBackColor = true;
+        this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+        // 
+        // btnStop
+        // 
+        this.btnStop.Location = new System.Drawing.Point(132, 127);
+        this.btnStop.Name = "btnStop";
+        this.btnStop.Size = new System.Drawing.Size(94, 29);
+        this.btnStop.TabIndex = 4;
+        this.btnStop.Text = "Stop";
+        this.btnStop.UseVisualStyleBackColor = true;
+        this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+        // 
+        // uiTimer
+        // 
+        this.uiTimer.Interval = 500;
+        this.uiTimer.Tick += new System.EventHandler(this.uiTimer_Tick);
+        // 
+        // Form1
+        // 
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.ClientSize = new System.Drawing.Size(340, 190);
+        this.Controls.Add(this.btnStop);
+        this.Controls.Add(this.btnStart);
+        this.Controls.Add(this.chkEnableInput);
+        this.Controls.Add(this.lblMouse);
+        this.Controls.Add(this.lblKeys);
+        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        this.MaximizeBox = false;
+        this.Name = "Form1";
+        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        this.Text = "ChildGuard UI";
+        this.ResumeLayout(false);
+        this.PerformLayout();
     }
 
     #endregion
--- ChildGuard.UI/Form1.cs
+++ ChildGuard.UI/Form1.cs
@@ -1,9 +1,50 @@
+using ChildGuard.Core.Configuration;
+using ChildGuard.Core.Models;
+using ChildGuard.Hooking;
+
 namespace ChildGuard.UI;
 
 public partial class Form1 : Form
 {
+    private readonly HookManager _hookManager = new();
+    private long _lastKeys;
+    private long _lastMouse;
+    private volatile bool _running;
+
     public Form1()
     {
         InitializeComponent();
+        uiTimer.Start();
+        _hookManager.OnEvent += OnActivity;
+    }
+
+    private void OnActivity(ActivityEvent evt)
+    {
+        if (evt.Data is InputActivitySummary s)
+        {
+            Interlocked.Exchange(ref _lastKeys, s.KeyPressCount);
+            Interlocked.Exchange(ref _lastMouse, s.MouseEventCount);
+        }
+    }
+
+    private void uiTimer_Tick(object? sender, EventArgs e)
+    {
+        lblKeys.Text = $"KeyPress Count: {_lastKeys}";
+        lblMouse.Text = $"Mouse Event Count: {_lastMouse}";
+    }
+
+    private void btnStart_Click(object? sender, EventArgs e)
+    {
+        if (_running) return;
+        var cfg = new AppConfig { EnableInputMonitoring = chkEnableInput.Checked };
+        _hookManager.Start(cfg);
+        _running = true;
+    }
+
+    private void btnStop_Click(object? sender, EventArgs e)
+    {
+        if (!_running) return;
+        _hookManager.Stop();
+        _running = false;
     }
 }
