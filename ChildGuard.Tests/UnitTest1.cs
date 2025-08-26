a28ae45e4ba40b34cee0cf806fc475bb319ac7ea
Bach Minh Quang <0947556012abc@gmail.com>
2025-08-26 18:07:31 +0700

    Tests: add JSONL sink test

--- ChildGuard.Tests/UnitTest1.cs
+++ ChildGuard.Tests/UnitTest1.cs
@@ -1,10 +1,22 @@
+using ChildGuard.Core.Models;
+using ChildGuard.Core.Sinks;
+
 namespace ChildGuard.Tests;
 
-public class UnitTest1
+public class CoreModelsTests
 {
     [Fact]
-    public void Test1()
+    public async Task JsonlSink_Writes_Line()
     {
+        var tmp = Path.Combine(Path.GetTempPath(), "childguard_test", Guid.NewGuid() + ".log");
+        var sink = new JsonlFileEventSink(tmp);
+        var evt = new ActivityEvent(DateTimeOffset.UnixEpoch, ActivityEventType.ActiveWindow, new ActiveWindowInfo("Title", "proc", 123));
+        await sink.WriteAsync(evt);
+        await sink.DisposeAsync();
 
+        Assert.True(File.Exists(tmp));
+        var content = await File.ReadAllTextAsync(tmp);
+        Assert.Contains("\"ActiveWindow\"", content);
+        Assert.Contains("\"Title\"", content);
     }
 }
